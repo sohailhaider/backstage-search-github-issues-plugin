6b34b7ef34f2e77179e68bf072c6b26b6537b718
Sohail Haider <sohailh343@gmail.com>
2021-10-19 17:00:22 +0200

    tests added for githubissues client

---
+++ plugins/github-issues/src/utilities/GitHubIssuesClient.test.ts
@@ -0,0 +1,40 @@
+// import { Entity } from '@backstage/catalog-model';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { GitHubIssueApiEntity } from '../types/api';
+import { GitHubIssuesClient } from './GitHubIssuesClient';
+// import { CATALOG_FILTER_EXISTS, CatalogListResponse } from './types/api';
+// import { DiscoveryApi } from './types/discovery';
+import { mockGitHubData } from '../mock-data/api-limit-mock';
+
+const server = setupServer();
+const mockBaseUrl = 'https://api.github.com/repos/backstage/backstage/issues';
+
+describe('CatalogClient', () => {
+  let client: GitHubIssuesClient;
+
+  beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+  afterAll(() => server.close());
+  afterEach(() => server.resetHandlers());
+
+  beforeEach(() => {
+    client = new GitHubIssuesClient();
+  });
+
+  describe('getEntities', () => {
+    const defaultServiceResponse: GitHubIssueApiEntity[] = [...mockGitHubData];
+
+    beforeEach(() => {
+      server.use(
+        rest.get(`${mockBaseUrl}`, (_, res, ctx) => {
+          return res(ctx.json(defaultServiceResponse));
+        }),
+      );
+    });
+
+    it('should fetch entities from correct endpoint', async () => {
+      const response = await client.getIssues('backstage/backstage');
+      expect(response).toEqual(defaultServiceResponse);
+    });
+  });
+});
